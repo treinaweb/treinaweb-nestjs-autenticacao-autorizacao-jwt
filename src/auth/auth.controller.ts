8b569eb4199ff870cb76c5713b3759899a4cb61b
Wesley Gado <wesleygado@MacBook-Air-de-Wesley.local>
2023-08-10 13:54:52 -0300

    configurando strategy e gerando token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() userDto: CreateUserDto) {
+    return this.authService.validarUsuario(userDto.email, userDto.password);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import { LocalStrategy } from './local.auth';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), JwtModule],
-  providers: [AuthService, UsersService],
+  providers: [AuthService, UsersService, LocalStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
